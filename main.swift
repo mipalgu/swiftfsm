fce4c8e9a4b4f0319548f5a9d4dc445645f0966e
Rene Hexel <r.hexel@griffith.edu.au>
2015-08-14 16:00:24 +1000

    Add trivial swiftfsm (non-test) target

---
+++ main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  swiftfsm
+//
+//  Created by Rene Hexel on 14/08/2015.
+//  Copyright © 2015 MiPal. All rights reserved.
+//
+
+import Darwin
+
+print("Hello, when I grow up, I will be a full-blown state machine scheduler!")
