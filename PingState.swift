9d2b0f0eccf96672f1a3e8195c285e7e8d98758e
Callum McColl <callum.mccoll1@gmail.com>
2015-08-18 15:37:39 +1000

    Added Ping and Pong state

---
+++ PingState.swift
@@ -0,0 +1,15 @@
+//
+//  PingState.swift
+//  swiftfsm
+//
+//  Created by Callum McColl on 12/08/2015.
+//  Copyright © 2015 MiPal. All rights reserved.
+//
+
+public class PingState: EmptyState {
+    
+    public func onEntry() {
+        print("ping\n")
+    }
+    
+}
---
+++ PongState.swift
@@ -0,0 +1,15 @@
+//
+//  PongState.swift
+//  swiftfsm
+//
+//  Created by Callum McColl on 12/08/2015.
+//  Copyright © 2015 MiPal. All rights reserved.
+//
+
+public class PongState: EmptyState {
+    
+    public func onEntry() {
+        print("pong\n")
+    }
+    
+}
