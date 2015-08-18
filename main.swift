62c543d666ad9c9f6431b4ee36f58ddc1ad58253
Callum McColl <callum.mccoll1@gmail.com>
2015-08-18 17:47:35 +1000

    Removed start method from main

--- main.swift
+++ main.swift
@@ -8,28 +8,26 @@
 
 import Darwin
 
-func start() -> Void {
-    print("Hello, when I grow up, I will be a full-blown state machine scheduler!")
+print("Hello, when I grow up, I will be a full-blown state machine scheduler!")
     
-    var states: [State] = [
-        PingState(name: "ping"),
-        PongState(name: "pong")
-    ]
+var states: [State] = [
+    PingState(name: "ping"),
+    PongState(name: "pong")
+]
     
-    var transitions: [Transition] = [
-        PersistentTransition(sourceState: states[0], targetState: states[1]),
-        PersistentTransition(sourceState: states[1], targetState: states[0])
-    ]
+var transitions: [Transition] = [
+    PersistentTransition(sourceState: states[0], targetState: states[1]),
+    PersistentTransition(sourceState: states[1], targetState: states[0])
+]
     
-    states[0].attachTransition(transitions[0])
-    states[1].attachTransition(transitions[1])
+states[0].attachTransition(transitions[0])
+states[1].attachTransition(transitions[1])
     
-    var fsm: FSMType = FSM(
-        initialState: states[0],
-        ringlet: StandardRinglet()
-    )
+var fsm: FSMType = FSM(
+    initialState: states[0],
+    ringlet: StandardRinglet()
+)
     
-    let scheduler: Scheduler = Scheduler()
-    scheduler.addMachine(fsm)
-    scheduler.run()
-}
+let scheduler: Scheduler = Scheduler()
+scheduler.addMachine(fsm)
+scheduler.run()
