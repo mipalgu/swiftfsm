b3f127c1aec8fb938552edb37c6b7ac7268fbbd7
Callum McColl <callum.mccoll1@gmail.com>
2015-08-19 20:32:52 +1000

    Added MachineLoader.swift

---
+++ MachineLoader.swift
@@ -0,0 +1,15 @@
+//
+//  MachineLoader.swift
+//  swiftfsm
+//
+//  Created by Callum McColl on 19/08/2015.
+//  Copyright © 2015 MiPal. All rights reserved.
+//
+
+import Swift_FSM
+
+protocol MachineLoader {
+    
+    func load(name: String) -> FSMType?
+    
+}
