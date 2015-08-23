baa4fabace0b4592cad573362c0f7dfb9219be92
Callum McColl <callum.mccoll1@gmail.com>
2015-08-23 15:27:33 +1000

    Added Restartable

---
+++ Restartable.swift
@@ -0,0 +1,13 @@
+//
+//  Restartable.swift
+//  swiftfsm
+//
+//  Created by Callum McColl on 23/08/2015.
+//  Copyright © 2015 MiPal. All rights reserved.
+//
+
+public protocol Restartable {
+    
+    func restart() -> Void
+    
+}
