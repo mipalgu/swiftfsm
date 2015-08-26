aa10dd58bd0aa8f1c6a5efdb000491fb1fb62745
Callum McColl <callum.mccoll1@gmail.com>
2015-08-26 17:18:23 +1000

    Added DLMachineLoader

---
+++ DLMachineLoader.swift
@@ -0,0 +1,15 @@
+//
+//  DLMachineLoader.swift
+//  swiftfsm
+//
+//  Created by Callum McColl on 26/08/2015.
+//  Copyright © 2015 MiPal. All rights reserved.
+//
+
+public class DLMachineLoader: MachineLoader {
+    
+    public func load(name: String) -> FiniteStateMachine? {
+        return nil
+    }
+    
+}
