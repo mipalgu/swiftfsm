b5ad09e119cc9d14cb62d1135f86798352e3a5dd
Callum McColl <callum.mccoll1@gmail.com>
2015-08-26 17:30:26 +1000

    Added LibraryManager and LibraryResource

---
+++ LibraryManager.swift
@@ -0,0 +1,14 @@
+//
+//  LibraryManager.swift
+//  swiftfsm
+//
+//  Created by Callum McColl on 26/08/2015.
+//  Copyright © 2015 MiPal. All rights reserved.
+//
+
+public protocol LibraryManager {
+    
+    func open(path: String) -> LibraryResource
+    func close(resource: LibraryResource)
+    
+}
---
+++ LibraryResource.swift
@@ -0,0 +1,15 @@
+//
+//  LibraryResource.swift
+//  swiftfsm
+//
+//  Created by Callum McColl on 26/08/2015.
+//  Copyright © 2015 MiPal. All rights reserved.
+//
+
+public protocol LibraryResource {
+    
+    func getSymbolPointer(symbol: String) -> UnsafeMutablePointer<Void>
+    func hasError() -> Bool
+    func getError() -> String?
+
+}
