3c3695470caebace6698dba3b890e2f2288fd4d9
Callum McColl <callum.mccoll1@gmail.com>
2015-11-28 10:49:35 +1000

    RChanged TeleportingTurtle so that it complies with the algorithm and removed the changes that I added

--- TeleportingTurtleGenerator.swift
+++ TeleportingTurtleGenerator.swift
@@ -70,8 +70,30 @@ public class TeleportingTurtleGenerator: FSMKripkeStateGenerator {
      *  Generate a kripke structure from the initial state of the finite state
      *  machine.
      */
-    public func generateFromFSM(fsm: FiniteStateMachine) -> KripkeState {
-        return self.buildKripkeStructure(self.buildStatePath(fsm))
+    public func generateFromFSM(var fsm: FiniteStateMachine) -> KripkeState {
+        var turtle: KripkeState = self.convertToKripkeState(fsm.currentState)
+        fsm.next()
+        var rabbit: KripkeState = self.convertToKripkeState(fsm.currentState)
+        turtle.target = rabbit
+        var power: Int = 1
+        var length: Int = 1
+        let initialState: KripkeState = turtle
+        while(turtle != rabbit) {
+            if (power == length) {
+                turtle = rabbit
+                power *= 2
+                length = 0
+            }
+            fsm.next()
+            let temp: KripkeState = self.convertToKripkeState(fsm.currentState)
+            rabbit.target = temp
+            rabbit = temp
+            length++
+            if (true == fsm.hasFinished()) {
+                break
+            }
+        }
+        return initialState
     }
     
     private func convertToKripkeState(state: State) -> KripkeState {
@@ -79,66 +101,6 @@ public class TeleportingTurtleGenerator: FSMKripkeStateGenerator {
             state: state,
             properties: self.extractor.extract(state)
         )
-    }
-    
-    private func buildStatePath(var fsm: FiniteStateMachine) -> [KripkeState] {
-        var turtle: KripkeState = self.convertToKripkeState(fsm.currentState)
-        fsm.next()
-        var rabbit: KripkeState = self.convertToKripkeState(fsm.currentState)
-        var history: [KripkeState] = [turtle, rabbit]
-        var power: Int = 1
-        var length: Int = 1
-        if (true == fsm.hasFinished() || turtle == rabbit) {
-            return history
-        }
-        repeat {
-            repeat {
-                if (power == length) {
-                    turtle = rabbit
-                    power *= 2
-                    length = 0
-                }
-                fsm.next()
-                rabbit = self.convertToKripkeState(fsm.currentState)
-                length++
-                history.append(rabbit)
-                if (true == fsm.hasFinished()) {
-                    return history
-                }
-            } while(turtle != rabbit)
-        } while (false == self.isCycle(history, length: length))
-        return history
-    }
-    
-    private func isCycle(history: [KripkeState], length: Int) -> Bool {
-        var turtle: KripkeState = history[0]
-        var rabbit: KripkeState = history[length]
-        var ti: Int = 0
-        var hi: Int = length
-        while (turtle != rabbit) {
-            turtle = history[++ti]
-            rabbit = history[++hi]
-        }
-        var i: Int = 0
-        while (turtle == rabbit && i++ < length && hi + 1 < history.count) {
-            turtle = history[++ti]
-            rabbit = history[++hi]
-        }
-        return turtle == rabbit
-    }
-    
-    private func buildKripkeStructure(path: [KripkeState]) -> KripkeState {
-        if (0 == path.count) {
-            return self.convertToKripkeState(EmptyState(name: "_empty"))
-        }
-        let initialState: KripkeState = path[0]
-        var last: KripkeState = initialState
-        for i in 1 ... path.count - 2 {
-            let temp: KripkeState = path[i]
-            last.target = temp
-            last = temp
-        }
-        return initialState
     }
     
 }
