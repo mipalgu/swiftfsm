4c23b22fda32feaab18fe1d495306fd0a185cbfe
Callum McColl <callum.mccoll1@gmail.com>
2015-12-05 23:47:12 +1000

    Removed print in KripkeState

--- KripkeState.swift
+++ KripkeState.swift
@@ -93,7 +93,6 @@ public class KripkeState: KripkeStateType {
             }
             let l: KripkeStateProperty = self.properties[key]!
             let r: KripkeStateProperty = other.properties[key]!
-            print("Check \(key)")
             if (false == (l == r)) {
                 return false
             }
