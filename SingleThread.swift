a03efb88e9ef5297e5748659e2cf578c4f326bec
Callum McColl <callum.mccoll1@gmail.com>
2016-01-11 21:41:49 +1000

    Added importing Darwin and Glibc in SingleThread.swift

--- SingleThread.swift
+++ SingleThread.swift
@@ -56,6 +56,12 @@
  *
  */
 
+ #if os(OSX)
+ import Darwin
+ #elseif os(Linux)
+ import Glibc
+ #endif
+
 public class SingleThread: Thread, ExecutingThread {
     
     public private(set) var currentlyRunning: Bool
