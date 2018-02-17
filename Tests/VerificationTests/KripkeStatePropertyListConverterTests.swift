dc47e744608f12f6e0c9952e32f3653a1dd0c510
Callum McColl <callum.mccoll1@gmail.com>
2018-02-17 23:52:25 +1000

    Test KripkeStatePropertyListConverter with primitive types

--- Tests/VerificationTests/KripkeStatePropertyListConverterTests.swift
+++ Tests/VerificationTests/KripkeStatePropertyListConverterTests.swift
@@ -69,16 +69,104 @@ public final class KripkeStatePropertyListConverterTests: VerificationTestCase {
         self.converter = KripkeStatePropertyListConverter()
     }
 
+    //swiftlint:disable:next function_body_length
     public func test_convertsSuccessfully() {
-        let list = ["b": KripkeStateProperty(
-            type: .Bool,
-            value: true
-        )]
+        let list = [
+            "b": KripkeStateProperty(
+                type: .Bool,
+                value: true
+            ),
+            "i": KripkeStateProperty(
+                type: .Int,
+                value: Int(1)
+            ),
+            "i8": KripkeStateProperty(
+                type: .Int8,
+                value: Int8(1)
+            ),
+            "i16": KripkeStateProperty(
+                type: .Int16,
+                value: Int16(1)
+            ),
+            "i32": KripkeStateProperty(
+                type: .Int32,
+                value: Int32(1)
+            ),
+            "i64": KripkeStateProperty(
+                type: .Int64,
+                value: Int64(1)
+            ),
+            "ui": KripkeStateProperty(
+                type: .UInt,
+                value: UInt(1)
+            ),
+            "ui8": KripkeStateProperty(
+                type: .UInt8,
+                value: UInt8(1)
+            ),
+            "ui16": KripkeStateProperty(
+                type: .UInt16,
+                value: UInt16(1)
+            ),
+            "ui32": KripkeStateProperty(
+                type: .UInt32,
+                value: UInt32(1)
+            ),
+            "ui64": KripkeStateProperty(
+                type: .UInt64,
+                value: UInt64(1)
+            ),
+            "f": KripkeStateProperty(
+                type: .Float,
+                value: Float(1.0)
+            ),
+            "f80": KripkeStateProperty(
+                type: .Float80,
+                value: Float80(1.0)
+            ),
+            "d": KripkeStateProperty(
+                type: .Double,
+                value: Double(1.0)
+            ),
+            "s": KripkeStateProperty(
+                type: .String,
+                value: "s"
+            )
+        ]
         let result = self.converter.convert(fromList: list)
         let expected: [String: Any] = [
-            "b": true
+            "b": true,
+            "i": Int(1),
+            "i8": Int8(1),
+            "i16": Int16(1),
+            "i32": Int32(1),
+            "i64": Int64(1),
+            "ui": UInt(1),
+            "ui8": UInt8(1),
+            "ui16": UInt16(1),
+            "ui32": UInt32(1),
+            "ui64": UInt64(1),
+            "f": Float(1.0),
+            "f80": Float80(1.0),
+            "d": Double(1.0),
+            "s": "s"
         ]
+        XCTAssertEqual(expected.count, result.count)
         XCTAssertEqual(expected["b"] as? Bool, result["b"] as? Bool)
+        XCTAssertEqual(expected["i"] as? Int, result["i"] as? Int)
+        XCTAssertEqual(expected["i8"] as? Int8, result["i8"] as? Int8)
+        XCTAssertEqual(expected["i16"] as? Int16, result["i16"] as? Int16)
+        XCTAssertEqual(expected["i32"] as? Int32, result["i32"] as? Int32)
+        XCTAssertEqual(expected["i64"] as? Int64, result["i64"] as? Int64)
+        XCTAssertEqual(expected["ui"] as? UInt, result["ui"] as? UInt)
+        XCTAssertEqual(expected["ui8"] as? UInt8, result["ui8"] as? UInt8)
+        XCTAssertEqual(expected["ui16"] as? UInt16, result["ui16"] as? UInt16)
+        XCTAssertEqual(expected["ui32"] as? UInt32, result["ui32"] as? UInt32)
+        XCTAssertEqual(expected["ui64"] as? UInt64, result["ui64"] as? UInt64)
+        XCTAssertEqual(expected["f"] as? Float, result["f"] as? Float)
+        XCTAssertEqual(expected["f80"] as? Float80, result["f80"] as? Float80)
+        XCTAssertEqual(expected["d"] as? Double, result["d"] as? Double)
+        XCTAssertEqual(expected["s"] as? String, result["s"] as? String)
     }
 
 }
