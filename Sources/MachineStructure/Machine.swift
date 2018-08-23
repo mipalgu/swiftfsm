c9abdd2cd2d80bfb5d9373d57f1e1e7ccff48a2b
Callum McColl <callum.mccoll1@gmail.com>
2018-08-23 10:41:08 +1000

    Added dependencies to Machine

--- Sources/MachineStructure/Machine.swift
+++ Sources/MachineStructure/Machine.swift
@@ -82,6 +82,8 @@ public struct Machine {
      * hierarchy.
      */
     public var fsm: AnyScheduleableFiniteStateMachine
+    
+    public var dependencies: [Dependency]
 
     /**
      *  Create a new `Machine`.
@@ -89,11 +91,13 @@ public struct Machine {
     public init(
         debug: Bool,
         name: String,
-        fsm: AnyScheduleableFiniteStateMachine
+        fsm: AnyScheduleableFiniteStateMachine,
+        dependencies: [Dependency] = []
     ) {
         self.debug = debug
         self.name = name
         self.fsm = fsm
+        self.dependencies = dependencies
     }
     
 }
