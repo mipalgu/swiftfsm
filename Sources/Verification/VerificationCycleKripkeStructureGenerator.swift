8ffb47319a210b82cfbee33a7a5dee5dfeb9cbf8
Callum McColl <callum.mccoll1@gmail.com>
2018-09-10 10:10:09 +1000

    Convert clones to array

--- Sources/Verification/VerificationCycleKripkeStructureGenerator.swift
+++ Sources/Verification/VerificationCycleKripkeStructureGenerator.swift
@@ -99,10 +99,10 @@ public final class VerificationCycleKripkeStructureGenerator<
             while let externals = spinner() {
                 // Clone all fsms.
                 let clones = job.tokens.enumerated().map {
-                    self.cloner.clone(
+                    Array(self.cloner.clone(
                         jobs: $1,
                         withLastRecords: job.lastRecords[$0]
-                    )
+                    ))
                 }
                 //let clones = self.cloner.clone(job.tokens)
                 // Execute.
