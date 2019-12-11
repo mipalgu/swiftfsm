e7c73fae29925fc5b9c7f86496f83b9053fc4eca
Callum McColl <callum.mccoll1@gmail.com>
2019-12-11 14:20:24 +1000

    Removed whiteboard dependencies from swiftfsm

--- Package.start.swift
+++ Package.start.swift
@@ -3,7 +3,6 @@
 import PackageDescription
 
 let normalDependencies: [Package.Dependency] = [
-    .package(url: "ssh://git.mipal.net/git/swift_wb.git", .branch("swift-4.2")),
     .package(url: "ssh://git.mipal.net/git/swift_CLReflect.git", .branch("master")),
     .package(url: "ssh://git.mipal.net/git/swift_helpers.git", .branch("master"))
 ]
--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@
 import PackageDescription
 
 let normalDependencies: [Package.Dependency] = [
-    .package(url: "ssh://git.mipal.net/git/swift_wb.git", .branch("swift-4.2")),
     .package(url: "ssh://git.mipal.net/git/swift_CLReflect.git", .branch("master")),
     .package(url: "ssh://git.mipal.net/git/swift_helpers.git", .branch("master"))
 ]
@@ -12,8 +11,10 @@ func convert(_ arr: [String]) -> [Target.Dependency] {
     return arr.map {.byName(name: $0) }
 }
 
-let foundationDeps: [Target.Dependency] = []
-let deps = normalDependencies
+let foundationDeps: [Target.Dependency] = [.byName(name: "Machines")]
+let deps = [
+    .package(url: "ssh://git.mipal.net/git/Machines.git", .branch("master")),
+] + normalDependencies
 
 let package = Package(
     name: "swiftfsm",
@@ -22,32 +23,31 @@ let package = Package(
             name: "swiftfsm",
             targets: ["swiftfsm_bin"]
         ),
-        /*.library(
+        .library(
             name: "CFSMs",
             type: .dynamic,
             targets: ["CFSMs"]
-        )*/
+        )
     ],
     dependencies: deps,
     targets: [
-        //.target(name: "CFSMs", dependencies: []),
+        .target(name: "CFSMs", dependencies: []),
         .target(name: "swiftfsm_helpers", dependencies: []),
         .target(name: "Gateways", dependencies: ["swift_helpers"]),
         .target(name: "Timers", dependencies: ["swiftfsm_helpers"]),
         .target(name: "Libraries", dependencies: ["IO"]),
-        .target(name: "MachineStructure", dependencies: convert(["Libraries", "GUSimpleWhiteboard", "Timers"]) + foundationDeps),
-        .target(name: "MachineLoading", dependencies: convert(["Libraries", "Gateways", "GUSimpleWhiteboard", "IO", "swift_helpers", "swiftfsm_helpers"]) + foundationDeps),
+        .target(name: "MachineStructure", dependencies: convert(["Libraries", "Timers"]) + foundationDeps),
+        .target(name: "MachineLoading", dependencies: convert(["Libraries", "Gateways", "IO", "swift_helpers", "swiftfsm_helpers"]) + foundationDeps),
         .target(name: "MachineCompiling", dependencies: convert(["IO"]) + foundationDeps),
-        .target(name: "Scheduling", dependencies: ["MachineStructure", "MachineLoading", "Timers", "GUSimpleWhiteboard", "Gateways"]),
+        .target(name: "Scheduling", dependencies: ["MachineStructure", "MachineLoading", "Timers", "Gateways"]),
         .target(name: "Verification", dependencies: ["IO", "MachineStructure", "Scheduling", "Timers", "Gateways", "swift_helpers"]),
         .target(name: "Parsing", dependencies: ["Scheduling", "Timers", "Verification"]),
-        .target(name: "CFSMWrappers", dependencies: ["GUSimpleWhiteboard", "Libraries", "Scheduling", "Timers"]),
+        .target(name: "CFSMWrappers", dependencies: ["Libraries", "Scheduling", "Timers"]),
         .target(
             name: "swiftfsm_bin",
             dependencies: [
-                "GUSimpleWhiteboard",
                 "IO",
-                //"CFSMs",
+                "CFSMs",
                 "swiftfsm_helpers",
                 "Libraries",
                 "MachineStructure",
