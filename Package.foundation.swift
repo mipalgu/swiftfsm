cb355e878b5573987094b90f31d3cf57c1ddf92d
Callum McColl <callum.mccoll1@gmail.com>
2020-01-01 12:38:08 +1000

    Add the IO target to the dependencies in both foundation and slim versions of the Package.swift

--- Package.foundation.swift
+++ Package.foundation.swift
@@ -1,4 +1,4 @@
-let foundationDeps: [Target.Dependency] = [.byName(name: "Machines")]
+let foundationDeps: [Target.Dependency] = [.byName(name: "Machines"), .buName(name: "IO")]
 let deps = [
     .package(url: "ssh://git.mipal.net/git/Machines.git", .branch("master")),
 ] + normalDependencies
--- Package.slim.swift
+++ Package.slim.swift
@@ -1,4 +1,4 @@
-let foundationDeps: [Target.Dependency] = [.byName(name: "swift_helpers")]
+let foundationDeps: [Target.Dependency] = [.byName(name: "swift_helpers"), .byName(name: "IO")]
 let deps = [
     .package(url: "ssh://git.mipal.net/git/swift_helpers.git", .branch("master"))
 ] + normalDependencies
