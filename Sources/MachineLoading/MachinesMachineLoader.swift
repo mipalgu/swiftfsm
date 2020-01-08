7b7e091e102dc7ef3d922211c8b5cddbe379874a
Callum McColl <callum.mccoll1@gmail.com>
2020-01-09 03:36:37 +1000

    Changed MachinesMachineLoader to use the new ShallowLibraryLoader and provide an implementation for when there is no foundation

--- Sources/MachineLoading/MachinesMachineLoader.swift
+++ Sources/MachineLoading/MachinesMachineLoader.swift
@@ -56,11 +56,14 @@
  *
  */
 
-#if !NO_FOUNDATION
+#if !NO_FOUNDATION && canImport(Foundation)
+
+import SwiftMachines
+
+#endif
 
 import FSM
 import Libraries
-import SwiftMachines
 import swiftfsm
 import IO
 import Gateways
@@ -71,9 +74,12 @@ public final class MachinesMachineLoader: MachineLoader {
 
     fileprivate typealias SymbolSignature = @convention(c) (Any, Any, Any) -> Any
 
+    #if !NO_FOUNDATION && canImport(Foundation)
     fileprivate let compiler: MachineCompiler<MachineAssembler>
-    fileprivate let libraryLoader: LibraryMachineLoader
     fileprivate let parser: MachineParser
+    #endif
+    
+    fileprivate let libraryLoader: ShallowLibraryMachineLoader
     fileprivate let printer: Printer
     
     fileprivate let buildDir: String
@@ -82,11 +88,12 @@ public final class MachinesMachineLoader: MachineLoader {
     fileprivate let linkerFlags: [String]
     fileprivate let swiftCompilerFlags: [String]
     fileprivate let swiftBuildFlags: [String]
-
+    
+    #if !NO_FOUNDATION && canImport(Foundation)
     @available(macOS 10.11, *)
     public init(
         compiler: MachineCompiler<MachineAssembler> = MachineCompiler(assembler: MachineAssembler()),
-        libraryLoader: LibraryMachineLoader,
+        loader: LibrarySymbolLoader,
         parser: MachineParser = MachineParser(),
         printer: Printer = CommandLinePrinter(errorStream: StderrOutputStream(), messageStream: StdoutOutputStream(), warningStream: StdoutOutputStream()),
         buildDir: String = ".build",
@@ -97,7 +104,7 @@ public final class MachinesMachineLoader: MachineLoader {
         swiftBuildFlags: [String] = []
     ) {
         self.compiler = compiler
-        self.libraryLoader = libraryLoader
+        self.libraryLoader = ShallowLibraryMachineLoader(loader: loader, printer: printer)
         self.parser = parser
         self.printer = printer
         self.buildDir = buildDir
@@ -107,39 +114,65 @@ public final class MachinesMachineLoader: MachineLoader {
         self.swiftCompilerFlags = swiftCompilerFlags
         self.swiftBuildFlags = swiftBuildFlags
     }
-
-    public func load<Gateway: FSMGateway>(name: String, gateway: Gateway, clock: Timer, path: String) -> (FSMType, [Dependency])? {
-        guard let machine = self.parser.parseMachine(atPath: path) else {
-            self.parser.errors.forEach(self.printer.error)
-            return nil
-        }
-        return load(machine: machine, gateway: gateway, clock: clock, prefix: name)
+    #else
+    
+    public init(
+        loader: LibrarySymbolLoader,
+        printer: Printer = CommandLinePrinter(errorStream: StderrOutputStream(), messageStream: StdoutOutputStream(), warningStream: StdoutOutputStream()),
+        buildDir: String = ".build",
+        cCompilerFlags: [String] = [],
+        cxxCompilerFlags: [String] = [],
+        linkerFlags: [String] = [],
+        swiftCompilerFlags: [String] = [],
+        swiftBuildFlags: [String] = []
+    ) {
+        self.libraryLoader = ShallowLibraryMachineLoader(loader: loader, printer: printer)
+        self.printer = printer
+        self.buildDir = buildDir
+        self.cCompilerFlags = cCompilerFlags
+        self.cxxCompilerFlags = cxxCompilerFlags
+        self.linkerFlags = linkerFlags
+        self.swiftCompilerFlags = swiftCompilerFlags
+        self.swiftBuildFlags = swiftBuildFlags
     }
+    
+    #endif
+    
+    public func load<Gateway: FSMGateway>(name: String, gateway: Gateway, clock: Timer, path: String) -> (FSMType, [Dependency])? {
+        // Attempt to load the machine from the compiled libs.
+        let buildDir = path + "/" + self.buildDir
+        if let data = self.loadCompiledMachine(name: name, gateway: gateway, clock: clock, path: buildDir, prefix: name) {
+            return data
+        }
+        #if !NO_FOUNDATION && canImport(Foundation)
+        // If we can't load the machine because it is not compiled, then compile and try to load it again.
+        if let machine = self.parser.parseMachine(atPath: path) {
+            return load(machine: machine, gateway: gateway, clock: clock, prefix: name)
+        }
+        self.parser.errors.forEach(self.printer.error)
+        #endif
+        return nil
+    }
+    
+#if !NO_FOUNDATION && canImport(Foundation)
 
     fileprivate func load<Gateway: FSMGateway>(machine: Machine, gateway: Gateway, clock: Timer, prefix: String, caller: FSM_ID? = nil) -> (FSMType, [Dependency])? {
         let dependantMachines = machine.submachines + machine.parameterisedMachines
-        let format: (String) -> String = {
-            if $0 == machine.name {
-                return prefix + "." + machine.name
-            }
-            return prefix + "." + machine.name + "." + $0
-        }
         let selfID: FSM_ID = gateway.id(of: prefix + "." + machine.name)
-        let dependantIds: [FSM_ID] = dependantMachines.map { gateway.id(of: format($0.name)) }
-        let callableIds = machine.callableMachines.map { gateway.id(of: format($0.name)) }
-        let invocableIds = machine.invocableMachines.map { gateway.id(of: format($0.name)) }
         let caller = caller ?? selfID
-        let newGateway = RestrictiveFSMGateway(
+        let newGateway = self.createRestrictiveGateway(
+            forMachine: machine.name,
             gateway: gateway,
-            selfID: caller,
-            callables: Set(callableIds + [selfID]),
-            invocables: Set(invocableIds),
-            whitelist: Set(dependantIds + [selfID]),
-            formatter: CallbackFormatter(format)
+            dependantMachines: dependantMachines.map { $0.name },
+            callableMachines: machine.callableMachines.map { $0.name },
+            invocableMachines: machine.invocableMachines.map { $0.name },
+            prefix: prefix,
+            selfID: selfID,
+            caller: caller
         )
         guard let recursed = dependantMachines.failMap({ (m: Machine) -> Dependency? in
-            let id = gateway.id(of: format(m.name))
-            let caller = true == callableIds.contains(id) ? caller : id
+            let id = newGateway.id(of: m.name)
+            let caller = true == machine.callableMachines.lazy.map { $0.name }.contains(m.name) ? caller : id
             return load(
                 machine: m,
                 gateway: gateway,
@@ -192,7 +225,74 @@ public final class MachinesMachineLoader: MachineLoader {
             return .invokableParameterisedMachine(fsm, dependencies)
         }
     }
+    
+#endif
+    
+    fileprivate func loadCompiledMachine<Gateway: FSMGateway>(name: String, gateway: Gateway, clock: Timer, path: String, prefix: String, caller: FSM_ID? = nil) -> (FSMType, [Dependency])? {
+        #if os(macOS)
+        let ext = "dylib"
+        #else
+        let ext = "so"
+        #endif
+        let libPath = path + "/" + name + "." + ext
+        guard let (fsm, dependencies) = self.libraryLoader.load(name: name, gateway: gateway, clock: clock, path: libPath) else {
+            return nil
+        }
+        let dependantMachines = dependencies.map { $0.name }
+        let callableMachines = dependencies.filter { $0.isCallable }.map { $0.name }
+        let invocableMachines = dependencies.filter { $0.isInvokable }.map {$0.name }
+        let selfID: FSM_ID = gateway.id(of: prefix + "." + name)
+        let caller = caller ?? selfID
+        let newGateway = self.createRestrictiveGateway(forMachine: name, gateway: gateway, dependantMachines: dependantMachines, callableMachines: callableMachines, invocableMachines: invocableMachines, prefix: prefix, selfID: selfID, caller: caller)
+        guard let allDependencies = dependencies.failMap({ (m: ShallowDependency) -> Dependency? in
+            let id = newGateway.id(of: m.name)
+            // Set the caller to the parent caller when we are calling machines.
+            let caller = true == callableMachines.contains(m.name) ? caller : id
+            let newDirectory = path + "/dependencies/" + m.name
+            guard let (fsm, dependencies) = self.loadCompiledMachine(name: m.name, gateway: gateway, clock: clock, path: newDirectory, prefix: prefix + "." + m.name, caller: caller) else {
+                return nil
+            }
+            switch m {
+            case .callableMachine:
+                guard let parameterisedMachine = fsm.asParameterisedFiniteStateMachine else {
+                    return nil
+                }
+                return Dependency.callableParameterisedMachine(parameterisedMachine, dependencies)
+            case .invokableMachine:
+                guard let parameterisedMachine = fsm.asParameterisedFiniteStateMachine else {
+                    return nil
+                }
+                return Dependency.invokableParameterisedMachine(parameterisedMachine, dependencies)
+            case .submachine:
+                guard let controllableMachine = fsm.asControllableFiniteStateMachine else {
+                    return nil
+                }
+                return Dependency.submachine(controllableMachine, dependencies)
+            }
+        }) else {
+            return nil
+        }
+        return (fsm, allDependencies)
+    }
+    
+    fileprivate func createRestrictiveGateway<Gateway: FSMGateway>(forMachine machine: String, gateway: Gateway, dependantMachines: [String], callableMachines: [String], invocableMachines: [String], prefix: String, selfID: FSM_ID, caller: FSM_ID?) -> RestrictiveFSMGateway<Gateway, CallbackFormatter> {
+        let format: (String) -> String = {
+            if $0 == machine {
+                return prefix + "." + machine
+            }
+            return prefix + "." + machine + "." + $0
+        }
+        let dependantIds: [FSM_ID] = dependantMachines.map { gateway.id(of: format($0)) }
+        let callableIds = callableMachines.map { gateway.id(of: format($0)) }
+        let invocableIds = invocableMachines.map { gateway.id(of: format($0)) }
+        return RestrictiveFSMGateway(
+            gateway: gateway,
+            selfID: caller ?? selfID,
+            callables: Set(callableIds + [selfID]),
+            invocables: Set(invocableIds),
+            whitelist: Set(dependantIds + [selfID]),
+            formatter: CallbackFormatter(format)
+        )
+    }
 
 }
-
-#endif
