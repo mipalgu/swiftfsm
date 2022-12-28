38089cfa388318deba5ba0661ad31573275c97f6
Callum McColl <callum.mccoll1@gmail.com>
2022-12-28 17:25:01 +1000

    Create conversion functions between FSMContext and StateContext.

--- Sources/FSM/FSMContext.swift
+++ Sources/FSM/FSMContext.swift
@@ -3,9 +3,19 @@ public struct FSMContext<Context: DataStructure, Environment: DataStructure> {
 
     public var state: Sendable
 
-    private var fsm: Context
+    public var fsm: Context
 
-    private var environment: Environment
+    public var environment: Environment
+
+    public init<StatesContext: DataStructure>(
+        stateContext: StateContext<StatesContext, Context, Environment>
+    ) {
+        self.init(
+            state: stateContext.state as Sendable,
+            fsm: stateContext.fsm,
+            environment: stateContext.environment
+        )
+    }
 
     public init(state: Sendable, fsm: Context, environment: Environment) {
         self.state = state
@@ -29,6 +39,14 @@ public struct FSMContext<Context: DataStructure, Environment: DataStructure> {
     public subscript<T>(dynamicMember keyPath: WritableKeyPath<Environment, T>) -> T {
         get { environment[keyPath: keyPath] }
         set { environment[keyPath: keyPath] = newValue }
+    }
+
+    public mutating func update<StatesContext: DataStructure>(
+        from stateContext: StateContext<StatesContext, Context, Environment>
+    ) {
+        self.state = stateContext.state as Sendable
+        self.fsm = stateContext.fsm
+        self.environment = stateContext.environment
     }
 
 }
--- Sources/FSM/StateContext.swift
+++ Sources/FSM/StateContext.swift
@@ -1,15 +1,25 @@
 @dynamicMemberLookup
 public struct StateContext<
-    StateContext: DataStructure, FSMContext: DataStructure, Environment: DataStructure
+    StateContext: DataStructure, FSMsContext: DataStructure, Environment: DataStructure
 > {
 
-    private var state: StateContext
+    public var state: StateContext
 
-    private var fsm: FSMContext
+    public var fsm: FSMsContext
 
-    private var environment: Environment
+    public var environment: Environment
 
-    public init(state: StateContext, fsm: FSMContext, environment: Environment) {
+    public init(fsmContext: FSMContext<FSMsContext, Environment>) {
+        // swiftlint:disable force_cast
+        self.init(
+            state: fsmContext.state as! StateContext,
+            fsm: fsmContext.fsm,
+            environment: fsmContext.environment
+        )
+        // swiftlint:enable force_cast
+    }
+
+    public init(state: StateContext, fsm: FSMsContext, environment: Environment) {
         self.state = state
         self.fsm = fsm
         self.environment = environment
@@ -24,11 +34,11 @@ public struct StateContext<
         set { state[keyPath: keyPath] = newValue }
     }
 
-    public subscript<T>(dynamicMember keyPath: KeyPath<FSMContext, T>) -> T {
+    public subscript<T>(dynamicMember keyPath: KeyPath<FSMsContext, T>) -> T {
         fsm[keyPath: keyPath]
     }
 
-    public subscript<T>(dynamicMember keyPath: WritableKeyPath<FSMContext, T>) -> T {
+    public subscript<T>(dynamicMember keyPath: WritableKeyPath<FSMsContext, T>) -> T {
         get { fsm[keyPath: keyPath] }
         set { fsm[keyPath: keyPath] = newValue }
     }
@@ -40,6 +50,14 @@ public struct StateContext<
     public subscript<T>(dynamicMember keyPath: WritableKeyPath<Environment, T>) -> T {
         get { environment[keyPath: keyPath] }
         set { environment[keyPath: keyPath] = newValue }
+    }
+
+    public mutating func update(from fsmContext: FSMContext<FSMsContext, Environment>) {
+        // swiftlint:disable force_cast
+        state = fsmContext.state as! StateContext
+        fsm = fsmContext.fsm
+        environment = fsmContext.environment
+        // swiftlint:enable force_cast
     }
 
 }
