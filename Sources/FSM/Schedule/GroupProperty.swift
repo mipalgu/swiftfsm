f0654fe0e666bbe0d496a10279cd7386b5b92f69
Callum McColl <callum.mccoll1@gmail.com>
2023-01-16 12:55:44 +1000

    Split ArrangementModel into ArrangementModel and ScheduleModel.

---
+++ Sources/FSM/Schedule/GroupProperty.swift
@@ -0,0 +1,16 @@
+@propertyWrapper
+public struct GroupProperty<Schedule: ScheduleModel> {
+
+    public let wrappedValue: (Schedule) -> GroupInformation
+
+    public init(slots keyPaths: KeyPath<Schedule, SlotProperty<Schedule.Arrangement>> ...) {
+        self.init(slots: keyPaths)
+    }
+
+    public init(slots keyPaths: [KeyPath<Schedule, SlotProperty<Schedule.Arrangement>>]) {
+        self.wrappedValue = { schedule in
+            GroupInformation(slots: keyPaths.map { schedule[keyPath: $0].wrappedValue(schedule.arrangement) })
+        }
+    }
+
+}
--- Tests/FSMTests/Mocks/FSM/ArrangementMock.swift
+++ Tests/FSMTests/Mocks/FSM/ArrangementMock.swift
@@ -1,9 +1,15 @@
 import FSM
 
-struct Arrangement: ArrangementModel {
+struct ArrangementMock: ArrangementModel {
 
     @Machine
     var pingPong = FSMMock()
+
+}
+
+struct Schedule: ScheduleModel {
+
+    let arrangement = ArrangementMock()
 
     @Slot(fsm: \.$pingPong, timing: (startTime: 0, duration: 20))
     var pingPongTimeslot
