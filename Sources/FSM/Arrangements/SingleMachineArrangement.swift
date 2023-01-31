fb07082b84c14089960138ee2de601fe7765d1c1
Callum McColl <callum.mccoll1@gmail.com>
2023-01-31 16:14:58 +1000

    Documented SingleMachineArrangement

--- Sources/FSM/Arrangements/SingleMachineArrangement.swift
+++ Sources/FSM/Arrangements/SingleMachineArrangement.swift
@@ -1,7 +1,12 @@
+/// An arrangement that contains only a single FSM.
 public struct SingleMachineArrangement: ArrangementProtocol {
 
+    /// An array that contains only a single FSM.
     public let fsms: [Machine]
 
+    /// Create a new SingleMachineArrangement.
+    ///
+    /// - Parameter fsm: The FSM that is contained within this arrangement.
     public init<FSM: FSMModel>(fsm: FSM) {
         self.fsms = [Machine(wrappedValue: fsm)]
     }
