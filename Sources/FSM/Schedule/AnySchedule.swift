73601ed932e528f20e2c50c602c19c4a8d1bcfb6
Callum McColl <callum.mccoll1@gmail.com>
2023-02-16 12:58:28 +1000

    Add more docs

--- Sources/FSM/Schedule/AnySchedule.swift
+++ Sources/FSM/Schedule/AnySchedule.swift
@@ -1,13 +1,32 @@
+/// A type-erased `ScheduleProtocol` that contains information regarding when and in what order to execute finite state machines within an
+/// `ArrangementProtocol`.
+///
+/// The schedule is composed of groups that represent separate threads of execution. Each group contains a sequence of slots that are executed
+/// sequentially, and in order.
 public struct AnySchedule<Arrangement: ArrangementProtocol>: ScheduleProtocol {
 
+    /// The arrangement that contains the finite state machines that comprise this schedule.
     public var arrangement: Arrangement
 
+    /// All groups that comprise this schedule.
+    ///
+    /// Each group represents a single thread of execution, where each thread executes a sequence of finite state machines sequentially.
     public var groups: [GroupInformation]
 
+    /// Create a schedule containing a single group comprised of the given slots.
+    ///
+    /// - Parameter arrangement: The arrangement that contains the finite state machines that are to comprise this schedule.
+    ///
+    /// - Parameter slots: The slots that will be converted to a single `GroupInformation`.
     public init(arrangement: Arrangement, slots: [SlotInformation]) {
         self.init(arrangement: arrangement, groups: [GroupInformation(slots: slots)])
     }
 
+    /// Create a new AnySchedule.
+    ///
+    /// - Parameter arrangement: The arrangement that contains the finite state machines that are to comprise this schedule.
+    ///
+    /// - Parameter slots: The groups that contains the slots to be executed.
     public init(arrangement: Arrangement, groups: [GroupInformation]) {
         self.arrangement = arrangement
         self.groups = groups
--- Sources/FSM/Schedule/GroupInformation.swift
+++ Sources/FSM/Schedule/GroupInformation.swift
@@ -1,9 +1,45 @@
+/// A data structure that contains metadata relating to a group that executes within a schedule.
+///
+/// A group within a schedule represents a single thread of execution. This single thread of execution executes a sequence of finite state machines
+/// sequentially, and in order. This data structure is responsible for defining the sequence of slots, where each slot contains metadata about the finite
+/// state machine that occupies it, that comprise the group.
 public struct GroupInformation: DataStructure {
 
+    /// The slots that belong to this group.
+    ///
+    /// Each slot contains metadata associated with a single fintie state machine, essentially providing information about when to execute a ringlet
+    /// for the finite state machine within the schedule.
     public let slots: [SlotInformation]
 
+    /// The amount of time it takes to execute this group.
+    ///
+    /// When using a non-time-trigggered schedule, this value is nil. When using a time-triggered schedule, this value should be at least as big as
+    /// the total duration of executing all slots within the schedule. This value can be bigger than the total duration of the slots, by doing so, this
+    /// implies that the group should sleep until the final duration time has expired after executing the final slot within the `slots` array.
     public let duration: UInt?
 
+    /// Create a new GroupInformation and mannualy override the default duration time to allow a greater duration time than the total duration of
+    /// executing `slots`.
+    ///
+    /// - Parameter slots: The slots that comprise this group.
+    ///
+    /// - Parameter duration: Manually set a duration value that overrides the default calculated duration that is derived from the total
+    /// execution time for executing all slots sequentially.
+    ///
+    /// - Warning: The given duration should at least be greater than or equal to the total duration of executing `slots`.
+    public init(slots: [SlotInformation], duration: UInt?) {
+        self.slots = slots
+        self.duration = duration
+    }
+    
+    /// Create a new GroupInformation, calculating the value of `duration`.
+    ///
+    /// If the slots contain start and end times, then this initialiser calculates the value of `duration` to be the gretest end time of all provided
+    /// slots. If the slots do not contain a start time and end time, then `duration` will be set to nil.
+    ///
+    /// - Parameter slots: The slots that comprise this group.
+    ///
+    /// - SeeAlso: `SlotInformation`.
     public init(slots: [SlotInformation]) {
         self.slots = slots
         let endSlots = slots.compactMap(\.endTime)
