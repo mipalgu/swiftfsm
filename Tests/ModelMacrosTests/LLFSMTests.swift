a64d06c5c63d277dd58d934a93c1346da8ca00a7
Callum McColl <callum.mccoll1@gmail.com>
2024-03-10 13:57:46 +1000

    Create LLFSMTests.

---
+++ Tests/ModelMacrosTests/LLFSMTests.swift
@@ -0,0 +1,179 @@
+import SwiftSyntaxMacros
+import SwiftSyntaxMacrosTestSupport
+import XCTest
+
+#if canImport(ModelMacros)
+@testable import ModelMacros
+#endif
+
+final class LLFSMTests: XCTestCase {
+
+    func test_generatesCorrectCode() throws {
+        #if canImport(ModelMacros)
+        let src = """
+            @LLFSM
+            public struct FSMMock {
+
+                @Context
+                public struct Context {
+
+                    public var fsmCount: Int = 0
+
+                }
+
+                @EnvironmentSnapshot
+                public struct Environment {
+
+                    @WriteOnly
+                    public var exitActuator: Bool!
+
+                    @ReadWrite
+                    public var exitExternalVariable: Bool!
+
+                    @ReadWrite
+                    public var exitGlobalVariable: Bool!
+
+                    @ReadOnly
+                    public var exitSensor: Bool!
+                }
+
+                @StateContext
+                public struct PangData {
+
+                    public var stateCount: Int = 0
+
+                }
+
+                @State(
+                    name: "Ping",
+                    onExit: { $0.fsmCount += 1 },
+                    transitions: {
+                        Transition(to: \\.$pong)
+                    }
+                )
+                public var ping
+
+                @State(
+                    name: "Pong",
+                    onExit: { $0.fsmCount += 1 },
+                    transitions: {
+                        Transition(to: \\.$pang)
+                    }
+                )
+                public var pong
+
+                @State(
+                    name: "Pang",
+                    initialContext: PangData(),
+                    uses: \\.$exitSensor,
+                    onEntry: { $0.stateCount = 0 },
+                    internal: {
+                        $0.stateCount += 1
+                        $0.fsmCount += 1
+                    },
+                    transitions: {
+                        Transition(to: \\.$exit, context: PangData.self) { $0.exitSensor }
+                        Transition(to: "Ping", context: PangData.self) { $0.stateCount > 5 }
+                    }
+                )
+                public var pang
+
+                @State(name: "Exit", onEntry: { _ in print("Exit") })
+                public var exit
+
+                public let initialState = #state(\\.$ping)
+
+                public init() {}
+
+            }
+            """
+        let expected = """
+            public struct FSMMock: LLFSM {
+
+                let initialContext = Context()
+
+                @Context
+                public struct Context {
+
+                    public var fsmCount: Int = 0
+
+                }
+
+                @EnvironmentSnapshot
+                public struct Environment {
+
+                    @WriteOnly
+                    public var exitActuator: Bool!
+
+                    @ReadWrite
+                    public var exitExternalVariable: Bool!
+
+                    @ReadWrite
+                    public var exitGlobalVariable: Bool!
+
+                    @ReadOnly
+                    public var exitSensor: Bool!
+                }
+
+                @StateContext
+                public struct PangData {
+
+                    public var stateCount: Int = 0
+
+                }
+
+                @State(
+                    name: "Ping",
+                    onExit: { $0.fsmCount += 1 },
+                    transitions: {
+                        Transition(to: \\.$pong)
+                    }
+                )
+                public var ping
+
+                @State(
+                    name: "Pong",
+                    onExit: { $0.fsmCount += 1 },
+                    transitions: {
+                        Transition(to: \\.$pang)
+                    }
+                )
+                public var pong
+
+                @State(
+                    name: "Pang",
+                    initialContext: PangData(),
+                    uses: \\.$exitSensor,
+                    onEntry: { $0.stateCount = 0 },
+                    internal: {
+                        $0.stateCount += 1
+                        $0.fsmCount += 1
+                    },
+                    transitions: {
+                        Transition(to: \\.$exit, context: PangData.self) { $0.exitSensor }
+                        Transition(to: "Ping", context: PangData.self) { $0.stateCount > 5 }
+                    }
+                )
+                public var pang
+
+                @State(name: "Exit", onEntry: { _ in print("Exit") })
+                public var exit
+
+                public let initialState = StateInformation(id: 0, name: "Ping")
+
+                public init() {}
+
+            }
+            """
+        assertMacroExpansion(
+            src,
+            expandedSource: expected,
+            macros: ["LLFSM": LLFSMMacro.self]
+        )
+        #else
+        throw XCTSkip("macros are only supported when running tests for the host platform")
+        #endif
+    }
+
+
+}
